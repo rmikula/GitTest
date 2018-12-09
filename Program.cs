569e82fcc2e748c11c2bb25e9afc502506938a6c
Roman Mikula <rmikula@gmail.com>
2018-12-09 20:56:46 +0100

    New class

--- Program.cs
+++ Program.cs
@@ -7,6 +7,8 @@ namespace GitTest
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            Console.WriteLine(new Person());
         }
     }
 }
---
+++ src/Person.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace GitTest
+{
+    public class Person
+    {
+        public string Name { get; set; }
+
+        public override string ToString()
+        {
+            return String.Empty;
+        }
+    }
+}
